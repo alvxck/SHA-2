b18675bd2c6deddd3b9e5ae5032bc20a182765ac
Alex <alexander_sck@live.ca>
2022-04-28 22:39:50 -0400

    refactor: organize function layout in all files

--- hashs/sha2.py
+++ hashs/sha2.py
@@ -1,3 +1,46 @@
+# SHA-2 Algorithms 
+# -----------------------------------------------------------------------------------------------------
+
+def sha224(message):
+    hash_constants = [
+        0xc1059ed8, 0x367cd507, 0x3070dd17, 0xf70e5939, 0xffc00b31, 0x68581511, 0x64f98fa7, 0xbefa4fa4
+        ]
+    return hash32(message, 224, hash_constants)
+
+def sha256(message):
+    hash_constants = [
+        0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+        ]
+    return hash32(message, 256, hash_constants)
+
+def sha384(message):
+    hash_constants = [
+        0xcbbb9d5dc1059ed8, 0x629a292a367cd507, 0x9159015a3070dd17, 0x152fecd8f70e5939, 
+        0x67332667ffc00b31, 0x8eb44a8768581511, 0xdb0c2e0d64f98fa7, 0x47b5481dbefa4fa4 
+    ]
+    return hash64(message, 384, hash_constants)
+
+def sha512(message):
+    hash_constants = [
+        0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1, 
+        0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x05be0cd19137e2179 
+    ]
+    return hash64(message, 512, hash_constants)
+
+def sha512_224(message):
+    hash_constants = [
+        0x8c3d37c819544da2, 0x73e1996689dcd4d6, 0x1dfab7ae32ff9c82, 0x679dd514582f9fcf,
+        0x0F6D2B697BD44DA8, 0x77e36f7304c48942, 0x3f9d85a86a1d36c8, 0x1112e6ad91d692a1
+    ]
+    return hash64(message, 224, hash_constants)
+
+def sha512_256(message):
+    hash_constants = [ 
+        0x22312194fc2bf72c, 0x9f555fa3c84c64c2, 0x2393b86b6f53b151, 0x963877195940eabd,
+        0x96283ee2a88effe3, 0xbe5e1e2553863992, 0x2b0199fc2c85b8aa, 0x0eb72ddc81c52ca2
+    ]
+    return hash64(message, 256, hash_constants)
+
 
 # Hash computation for 64-bit algorithms
 # -----------------------------------------------------------------------------------------------------
@@ -157,47 +200,3 @@ def hash32(message, bit_length, hash_constants):
 
     # 5. Digest Concatenation
     return ''.join([format(hash, '08x') for hash in hash_constants])[0 : bit_length//4]
-
-
-# SHA-2 Algorithms 
-# -----------------------------------------------------------------------------------------------------
-
-def sha224(message):
-    hash_constants = [
-        0xc1059ed8, 0x367cd507, 0x3070dd17, 0xf70e5939, 0xffc00b31, 0x68581511, 0x64f98fa7, 0xbefa4fa4
-        ]
-    return hash32(message, 224, hash_constants)
-
-def sha256(message):
-    hash_constants = [
-        0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
-        ]
-    return hash32(message, 256, hash_constants)
-
-def sha384(message):
-    hash_constants = [
-        0xcbbb9d5dc1059ed8, 0x629a292a367cd507, 0x9159015a3070dd17, 0x152fecd8f70e5939, 
-        0x67332667ffc00b31, 0x8eb44a8768581511, 0xdb0c2e0d64f98fa7, 0x47b5481dbefa4fa4 
-    ]
-    return hash64(message, 384, hash_constants)
-
-def sha512(message):
-    hash_constants = [
-        0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1, 
-        0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x05be0cd19137e2179 
-    ]
-    return hash64(message, 512, hash_constants)
-
-def sha512_224(message):
-    hash_constants = [
-        0x8c3d37c819544da2, 0x73e1996689dcd4d6, 0x1dfab7ae32ff9c82, 0x679dd514582f9fcf,
-        0x0F6D2B697BD44DA8, 0x77e36f7304c48942, 0x3f9d85a86a1d36c8, 0x1112e6ad91d692a1
-    ]
-    return hash64(message, 224, hash_constants)
-
-def sha512_256(message):
-    hash_constants = [ 
-        0x22312194fc2bf72c, 0x9f555fa3c84c64c2, 0x2393b86b6f53b151, 0x963877195940eabd,
-        0x96283ee2a88effe3, 0xbe5e1e2553863992, 0x2b0199fc2c85b8aa, 0x0eb72ddc81c52ca2
-    ]
-    return hash64(message, 256, hash_constants)
--- hashs/sha3.py
+++ hashs/sha3.py
@@ -1,31 +1,6 @@
-
-def keccak():
-    b = 1600
-    w = 64
-    l = 6
-    n = 24
-
-    # 1. State Array Construction
-
-    #2. Keccak-p Permutation
-    # k0 specification
-    # k1 specification
-    k1_offsets = {}
-    # k2 specification
-    # k3 specification
-    # k4 specification
-
-    # 3. State Array Decomposition
-
-def sponge(bit_length, message):
-    #1. Pre-Process 'message'
-    k = keccak()
-
-    #2. Truncate Loop
-
-
 # SHA-3 Algorithms 
 # -----------------------------------------------------------------------------------------------------
+
 def sha3_224(message):
     return sponge(224, message)
 
@@ -37,4 +12,36 @@ def sha3_384(message):
 
 def sha3_512(message):
     return sponge(512, message)
+
+# KECCAK-f[b] and Sponge Intermediates
+# -----------------------------------------------------------------------------------------------------
+
+def keccak():
+    b = 1600
+    w = 64
+    l = 6
+    n = 24
+
+    # 1. State Array Construction
+
+    # 2. Keccak-p Permutation
+    # k0 specification
+
+    # k1 specification
+    k1_offsets = {}
+
+    # k2 specification
+
+    # k3 specification
+
+    # k4 specification
+
+
+    # 3. State Array Decomposition
+
+def sponge(bit_length, message):
+    #1. Pre-Process 'message'
+    k = keccak()
+
+    #2. Truncate Loop
     
